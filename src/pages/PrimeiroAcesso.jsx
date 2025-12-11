1ab557e0a6d3ca6d2987273991606036009987fa
Guilherme Silva <bundagame124@gmail.com>
2025-12-11 05:33:06 +0000

    File changes

--- src/pages/PrimeiroAcesso.jsx
+++ src/pages/PrimeiroAcesso.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { createPageUrl } from "@/utils";
-import { base44 } from "@/api/base44Client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
