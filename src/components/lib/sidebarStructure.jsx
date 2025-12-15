119b59366583400d714816d3a0f56902244e2d6f
Aceleradora Oficinas Master <oficinasmaster@gmail.com>
2025-12-15 19:44:26 +0000

    File changes

--- src/components/lib/sidebarStructure.jsx
+++ src/components/lib/sidebarStructure.jsx
@@ -125,7 +125,6 @@ export const sidebarStructure = [
     items: [
       { name: "Usuários", description: "Gerenciar usuários" },
       { name: "Gestão de Perfis", description: "Perfis e permissões" },
-      { name: "Tela Inicial", description: "Widgets Home" },
       { name: "Permissões", description: "Controle de acesso" },
       { name: "Produtividade", description: "Métricas KPIs" },
       { name: "Desafios Globais", description: "Nível Brasil" },
