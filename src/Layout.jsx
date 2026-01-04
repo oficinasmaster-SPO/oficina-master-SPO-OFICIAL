7705afd33b0c3006517c42cba61fad68f2148dac
Aceleradora Oficinas Master <oficinasmaster@gmail.com>
2026-01-04 19:40:09 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -352,10 +352,11 @@ export default function Layout({ children, currentPageName }) {
                 </div>
               </div>
             </div>
-          </div>
-        </header>
+            </div>
+            </header>
+            )}
 
-        <main className="flex-1">
+            <main className="flex-1">
           <div className={`${isAuthenticated ? 'px-4 sm:px-6 lg:px-8 py-6' : ''}`}>
             {isAuthenticated && <Breadcrumbs />}
             {isAuthenticated ? (
