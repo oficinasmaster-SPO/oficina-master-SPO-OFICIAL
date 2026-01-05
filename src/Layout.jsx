f409ffbc4f6a7dfc6ca21b35c650f73e7593fbf8
Aceleradora Oficinas Master <oficinasmaster@gmail.com>
2026-01-05 11:31:37 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { base44 } from "@/api/base44Client";
-import { Bell, LogOut, Menu, X, LogIn } from "lucide-react";
+import { Bell, LogOut, Menu, X, LogIn, AlertCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { useQuery } from "@tanstack/react-query";
