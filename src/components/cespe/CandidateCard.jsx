67711fede685e01733239204ea3eb4a23f2765d8
Aceleradora Oficinas Master <oficinasmaster@gmail.com>
2026-01-05 22:50:25 +0000

    File changes

--- src/components/cespe/CandidateCard.jsx
+++ src/components/cespe/CandidateCard.jsx
@@ -118,7 +118,7 @@ export default function CandidateCard({ candidate }) {
             </Button>
           )}
           <Button 
-            onClick={() => window.location.href = `${createPageUrl("CESPEEntrevista")}?candidate_id=${candidate.id}`}
+            onClick={() => navigate(`${createPageUrl("CESPEEntrevista")}?candidate_id=${candidate.id}`)}
             size="sm"
             variant="outline"
           >
